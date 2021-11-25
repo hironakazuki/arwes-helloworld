2e02f615fe50d657325e0ac5729602a68f518e37
hironakazuki <far_east_nightbird30@icloud.com>
2021-11-25 20:28:11 +0900

    chore: tailwind-css install

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // mode: 'jit',
+  // purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
